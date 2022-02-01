4830e7d8d8a41380db1b8e20c2edf9807a566e39
innerseacn <42731705+innerseacn@users.noreply.github.com>
2022-02-01 22:39:34 +0800

    add posh theme

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,4 @@
-Set-Theme Honukai
-Import-Module posh-git
-Import-Module oh-my-posh
+oh-my-posh --init --shell pwsh --config C:\Users\InnerSea\pyprojs\autohotkey_scripts\.myposhtheme.json | Invoke-Expression
 Import-Module "$($(Get-Item $(Get-Command scoop).Path).Directory.Parent.FullName)\modules\scoop-completion"
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -EditMode Emacs
