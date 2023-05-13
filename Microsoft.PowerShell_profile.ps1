e2dd99ee10a7949d0d367fb345c54b0324e5a761
innersea <innersea@innersea.com>
2023-05-14 01:26:18 +0800

    update scoop config and pwsh config

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,6 +5,7 @@ Set-PSReadLineOption -PredictionSource History
 invoke-Expression (&starship init powershell)
 Invoke-Expression (& { (lua D:\gitcodes\z.lua\z.lua --init powershell enhanced) -join "`n" })
 Set-Alias "vi" "nvim"
+Function vim($file) {nvim --clean $file}
 # Import-Module DockerCompletion
 # oh-my-posh init pwsh --config D:\gitcodes\autohotkey_scripts\.myposhtheme.json | Invoke-Expression
 # function em {emacs -nw -Q}
